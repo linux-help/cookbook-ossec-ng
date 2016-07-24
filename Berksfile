03cb22c10fad5a2b0e9c23f897ffbea5ecb3da1f
Eric Renfro <psi-jack@linux-help.org>
2016-07-24 16:11:12 -0400

    Initial commit as ossec-ng

---
+++ Berksfile
@@ -0,0 +1,8 @@
+source "https://supermarket.chef.io"
+
+metadata
+
+cookbook "selinux_policy", "~> 0.9.5"
+cookbook "yum-atomic", "~> 0.1.2"
+cookbook "yum-epel"
+cookbook "apt-atomic", "~> 0.1.2"
---
+++ metadata.rb
@@ -0,0 +1,33 @@
+name             "ossec-ng"
+maintainer       "Eric Renfro"
+maintainer_email "psi-jack@linux-help.org"
+license          "GPLv2"
+description      "Installs/Configures ossec"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "1.2.0"
+issues_url       "http://git.linux-help.org/Linux-Help/ossec-ng/issues"
+source_url       "http://git.linux-help.org/Linux-Help/ossec-ng"
+
+recipe 'ossec-ng::default', 'Installs the neccessary repositories to install OSSEC'
+recipe 'ossec-ng::server', 'Installs OSSEC for use as a server'
+recipe 'ossec-ng::agent', 'Installs OSSEC for use as an agent'
+
+%w(
+  debian
+  ubuntu
+  centos
+  redhat
+  scientific
+  amazon
+  oracle
+).each do |os|
+  supports os
+end
+
+depends 'yum-epel'
+depends 'yum-atomic', '~> 0.1.2'
+depends 'apt-atomic', '~> 0.1.2'
+
+suggests 'postfix'
+suggests 'selinux_policy'
+
